d13196371e63ca13afc30b39bdb5403cf237438b
xamidovscode <xamidovcoder@gmail.com>
2024-07-05 11:05:58 +0500

    validation\

--- core/settings/production.py
+++ core/settings/production.py
@@ -17,7 +17,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_COOKIE_SECURE = True
 CSRF_TRUSTED_ORIGINS = [
-    "https://example.com"
+    'http://joxacode.uz',
+    'https://joxacode.uz'
 ]
 
 ###################################################################
