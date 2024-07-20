a08fc96b38dad1c17179e67a7daccb27bc21a0c8
xamidovscode <xamidovscode@gmail.com>
2024-07-20 16:50:53 +0500

    lists

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('create/student/', views.CreateStudentWithApartmentAPIView.as_view(), name='create_student'),
     path('student/list/', views.StudentsListAPIView.as_view(), name='login'),
     path('employee/list/', views.EmployeeListAPIView.as_view()),
-    path('students/<int:pk>/', views.StudentsListAPIView.as_view(), name='login'),
 
 ]
 
