49479b4a6406c2f502a8098434217cc061914669
xamidovscode <xamidovcoder@gmail.com>
2024-09-29 18:00:07 +0500

    new changes and api's

--- apps/common/serializers.py
+++ apps/common/serializers.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 from rest_framework.serializers import ModelSerializer
-from rest_framework.validators import ValidationError
 from ..common import models
 from rest_framework import serializers
 
